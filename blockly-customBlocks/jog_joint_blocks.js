c341a7f51f39e9dcefabb2f3388cd61df0b7e53d
burakaksoy <burakaksoy20@gmail.com>
2020-06-24 09:36:14 -0400

    Change some file names

---
+++ blockly-customBlocks/jog_joint_blocks.js
@@ -0,0 +1,83 @@
+Blockly.defineBlocksWithJsonArray([{
+  "type": "robot_jog_joint_not_relative",
+  "message0": "Jog Joint %1 to degree %2 %3",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "JOINT_SELECTED",
+      "options": [
+        ["1","1"],
+        ["2","2"],
+        ["3","3"],
+        ["4","4"],
+        ["5","5"],
+        ["6","6"],
+        ["7","7"]
+      ]
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "DEGREE",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 0,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "robot_jog_joint_relative",
+  "message0": "Jog Joint %1 by degree %2 %3 relatively.",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "JOINT_SELECTED",
+      "options": [
+        ["1","1"],
+        ["2","2"],
+        ["3","3"],
+        ["4","4"],
+        ["5","5"],
+        ["6","6"],
+        ["7","7"]
+      ]
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "DEGREE",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 0,
+  "tooltip": "",
+  "helpUrl": ""
+}]);
+
+
+Blockly.Python['robot_jog_joint_not_relative'] = function(block) {
+  var dropdown_joint_selected = block.getFieldValue('JOINT_SELECTED');
+  var value_degree = Blockly.Python.valueToCode(block, 'DEGREE', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = 'jog_joints2(' + dropdown_joint_selected + ', ' + value_degree + ', False)\n';
+  return code;
+};
+
+Blockly.Python['robot_jog_joint_relative'] = function(block) {
+  var dropdown_joint_selected = block.getFieldValue('JOINT_SELECTED');
+  var value_degree = Blockly.Python.valueToCode(block, 'DEGREE', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = 'jog_joints2(' + dropdown_joint_selected + ', ' + value_degree + ', True)\n';
+  return code;
+};
---
+++ blockly-customBlocks/jog_joint_blocks_gen.js
@@ -0,0 +1,15 @@
+Blockly.Python['robot_jog_joint_not_relative'] = function(block) {
+  var dropdown_joint_selected = block.getFieldValue('JOINT_SELECTED');
+  var value_degree = Blockly.Python.valueToCode(block, 'DEGREE', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Python['robot_jog_joint_relative'] = function(block) {
+  var dropdown_joint_selected = block.getFieldValue('JOINT_SELECTED');
+  var value_degree = Blockly.Python.valueToCode(block, 'DEGREE', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
